20ab16dc8610762c90afaad12e5b5f0ec32ed64f
@wandji20 <wbertrand10@gmail.com>
2023-02-04 23:55:00 +0100

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ class DatabaseBuilder
   end
 
   def create_users
-    500.times do
+    50.times do
       attr = {
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name
@@ -39,8 +39,8 @@ class DatabaseBuilder
           content_type: "application/#{File.extname(file).sub('.', '')}",
           identify: false
         })
+        io.rewind
       end
-      io.rewind
     end
   end
 
@@ -51,15 +51,16 @@ class DatabaseBuilder
   end
 
   def create_books
-    author_ids = User.order(id: :desc).take(50).pluck(:id)
+    author_ids = User.order(id: :desc).take(10).pluck(:id)
     library_ids = Library.pluck(:id)
     author_ids.each do |author_id|
-      rand(8..25).times do
+      rand(3..10).times do
         attrs = {
           author_id: author_id,
           title: Faker::Book.title,
           library_id: library_ids.sample,
-          published_year: (1927..2023),
+          published_year: rand(1927..2023),
+          read_period: rand(3..7)
         }
         book = Book.create!(attrs)
         if book.persisted?
@@ -72,27 +73,31 @@ class DatabaseBuilder
             content_type: "application/#{File.extname(file).sub('.', '')}",
             identify: false
           })
+          io.rewind
+          rand(5..10).times do
+            book.copies.create!
+          end
         end
-        io.rewind
       end
     end
   end
 
   def create_readings
-    user_ids = User.order(id: :asc).take(450).pluck(:id)
+    user_ids = User.order(id: :asc).take(40).pluck(:id)
     library_ids = Library.pluck(:id)
-    Book.all.shuffle.slice(rand(300..500), Book.count).each do |book|
-      rand(2..5).times do |i|
+    BookCopy.all.shuffle.slice(rand(50..100), BookCopy.count).each do |copy|
+      rand(2..15).times do |i|
         attr = {
-          book_id: book.id,
+          book_copy_id: copy.id,
           user_id: user_ids.sample,
-          library_id: book.library_id,
-          status: [0,1].sample,
-          use_period: rand(2..7),
+          library_id: copy.book.library_id,
+          status: [0,1,2,3].sample,
           signed_at: i.days.try([:ago, :from_now].sample),
           return_library_id: library_ids.sample
         }
-        attr[:returned_at] = attr[:signed_at] + rand(2..9).days
+        attr[:returned_at] = attr[:signed_at] + rand(1..8).days
+        attr[:to_return_at] = attr[:signed_at] + copy.book.read_period.days
+        attr[:status] = 1 if DateTime.now < attr[:to_return_at]
         Reading.create!(attr)
       end
     end
