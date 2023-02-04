d71183431d3e25af41b32233c5dea0504952ff78
@wandji20 <wbertrand10@gmail.com>
2023-02-05 00:16:23 +0100

    Add queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,31 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :books, [Types::BookType], null: true do
+      argument :library_id, ID, required: false
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def books(params = {})
+      Book.where(params)
+    end
+
+    field :most_popular, [Types::BookType] do
+      argument :library_id, ID, required: false
+    end
+
+    def most_popular(params = {})
+      Book.where(params).most_popular
+    end
+
+    field :book, Types::BookType, null: true do
+      argument :id, ID, required: true
+      argument :library_id, ID, required: false
+    end
+
+    def book(params)
+      Book.find_by(params)
     end
   end
 end
